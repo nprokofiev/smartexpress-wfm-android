3b3d95c5913d6b537d1b8fe36a21a551f5273bd7
Nikolay Prokofiev <nprokofiev@gmail.com>
2014-12-23 08:10:50 -0500

    registration

---
+++ src/main/java/ru/smartexpress/courierapp/model/Courier.java
@@ -0,0 +1,10 @@
+package ru.smartexpress.courierapp.model;
+
+/**
+ * courier-android
+ *
+ * @author <a href="mailto:nprokofiev@gmail.com">Nikolay Prokofiev</a>
+ * @date 23.12.14 8:03
+ */
+public class Courier {
+}

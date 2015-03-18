c3db287d1f89829c8e14730b292b685c8d2bc249
Nikolay Prokofiev <nprokofiev@gmail.com>
2015-03-18 14:46:11 -0400

    activity worflow

---
+++ src/main/java/ru/smartexpress/courierapp/activity/ActiveOrdersFragment.java
@@ -0,0 +1,10 @@
+package ru.smartexpress.courierapp.activity;
+
+/**
+ * courier-android
+ *
+ * @author <a href="mailto:nprokofiev@gmail.com">Nikolay Prokofiev</a>
+ * @date 18.03.15 11:54
+ */
+public class ActiveOrdersFragment {
+}
---
+++ src/main/java/ru/smartexpress/courierapp/order/UserDao.java
@@ -0,0 +1,10 @@
+package ru.smartexpress.courierapp.order;
+
+/**
+ * courier-android
+ *
+ * @author <a href="mailto:nprokofiev@gmail.com">Nikolay Prokofiev</a>
+ * @date 15.03.15 21:11
+ */
+public class UserDao {
+}

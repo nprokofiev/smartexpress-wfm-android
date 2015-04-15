b5f82dea58ea1f34896ac2ac8bd99e6a93fbbf7e
Nikolay Prokofiev <nprokofiev@gmail.com>
2015-04-15 08:35:05 -0400

    courier status

--- src/main/java/ru/smartexpress/courierapp/CommonConstants.java
+++ src/main/java/ru/smartexpress/courierapp/CommonConstants.java
@@ -10,9 +10,9 @@ public class CommonConstants {
     public static final String HOST ="10.0.2.2";
   public static final int PORT =20080;
     public static final String REST_URL = "http://"+HOST+":"+PORT+"/rest";
- //public static final String HOST = "api.smartexpress.ru";
- //public static final int PORT = 80;
- //public static final String REST_URL = "http://api.smartexpress.ru/se/rest"; //
+// public static final String HOST = "api.smartexpress.ru";
+// public static final int PORT = 80;
+// public static final String REST_URL = "http://api.smartexpress.ru/se/rest"; //
 
  public static final String REGISTRATION_ID_KEY="registrationId";
  public static final String USERNAME_KEY = "username";
---
+++ src/main/java/ru/smartexpress/courierapp/service/MobileMessageIntentService.java
@@ -0,0 +1,60 @@
+package ru.smartexpress.courierapp.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import ru.smartexpress.courierapp.CommonConstants;
+import ru.smartexpress.courierapp.receiver.GcmBroadcastReceiver;
+import ru.smartexpress.courierapp.service.notification.NotificationProcessor;
+
+/**
+ * courier-android
+ *
+ * @author <a href="mailto:nprokofiev@gmail.com">Nikolay Prokofiev</a>
+ * @date 24.01.15 18:03
+ */
+public class MobileMessageIntentService extends IntentService  {
+    public MobileMessageIntentService() {
+        super(CommonConstants.SENDER_ID);
+    }
+    public static final String TAG = "GcmIntentService";
+    private NotificationProcessor notificationProcessor;
+
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        notificationProcessor = new NotificationProcessor(this);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        // The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+        String messageType = gcm.getMessageType(intent);
+
+        if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
+            /*
+             * Filter messages based on message type. Since it is likely that GCM will be
+             * extended in the future with new message types, just ignore any message types you're
+             * not interested in, or that you don't recognize.
+             */
+            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                Log.i(TAG, "Send error: " + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
+                Log.i(TAG, "Deleted messages on server: " + extras.toString());
+                // If it's a regular GCM message, do some work.
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                notificationProcessor.processPendingMessages();
+                Log.i(TAG, "Received: " + extras.toString());
+            }
+        }
+        // Release the wake lock provided by the WakefulBroadcastReceiver.
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+}

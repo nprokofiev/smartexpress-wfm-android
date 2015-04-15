a82516919d5595c231d292e27b743cb44bd3b2b0
Nikolay Prokofiev <nprokofiev@gmail.com>
2015-04-15 08:34:29 -0400

    courier status

--- src/main/java/ru/smartexpress/courierapp/receiver/GcmBroadcastReceiver.java
+++ src/main/java/ru/smartexpress/courierapp/receiver/GcmBroadcastReceiver.java
@@ -5,7 +5,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
-import ru.smartexpress.courierapp.service.GcmIntentService;
+import ru.smartexpress.courierapp.service.MobileMessageIntentService;
 
 /**
  * courier-android
@@ -19,7 +19,7 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // Explicitly specify that GcmIntentService will handle the intent.
         ComponentName comp = new ComponentName(context.getPackageName(),
-                GcmIntentService.class.getName());
+                MobileMessageIntentService.class.getName());
         // Start the service, keeping the device awake while it is launching.
         startWakefulService(context, (intent.setComponent(comp)));
         setResultCode(Activity.RESULT_OK);
